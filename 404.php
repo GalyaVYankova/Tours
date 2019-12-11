e9fc2d2070255a9082edf4f83040270302c6aee5
GalyaYankova <GalyaYankova@github.com>
2019-12-11 20:42:07 +0200

    first commit

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+    <div class="container pt-5 pb-5">
+    <head class="page-header">
+        <h1 class="page-title">Грешка! Опитай пак :)</h1>
+    </head>
+    <div class="page-content">
+        <p>Страницата, която се опитвате да намерите не съществува</p>
+<?php get_search_form(); ?>
+</div>
+</div>
